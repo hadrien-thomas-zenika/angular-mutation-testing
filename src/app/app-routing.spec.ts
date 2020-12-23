d01c93f8d0ed15c35053dfbfe42f3967ce1d7dd1
Hadrien THOMAS <hadrien.thomas@zenika.com>
2020-12-23 12:03:01 +0100

    remove unecessary and bring tests to 100% coverage

---
+++ src/app/app-routing.spec.ts
@@ -0,0 +1,38 @@
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from './routes';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+describe('AppRouting', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes(routes)
+      ],
+    });
+  });
+
+  it('should have an initial navigation to root path', fakeAsync(() => {
+    const router: Router = TestBed.inject<Router>(Router);
+    const location: Location = TestBed.inject<Location>(Location);
+
+    router.initialNavigation();
+    tick();
+    const initialPath: string = location.path();
+    expect(initialPath).toEqual('/');
+  }));
+
+  it('should not route to anything because we have no route', fakeAsync(() => {
+    const router: Router = TestBed.inject<Router>(Router);
+    const location: Location = TestBed.inject<Location>(Location);
+
+    router.navigate([ '/notapath' ]).catch(() => {
+      // we expect to pass here
+      expect().nothing();
+    });
+    tick();
+    const pathAfterAnyNavigation: string = location.path();
+    expect(pathAfterAnyNavigation).toEqual('/');
+  }));
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+      imports: [],
       declarations: [
         AppComponent
       ],
@@ -18,18 +15,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'angular-mutation-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-mutation-testing');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-mutation-testing app is running!');
   });
 });
